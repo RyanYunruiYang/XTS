d80345fe1549a06bca59697619377cd378480a1a
Ryan Yang <y.ryan.yang@gmail.com>
2024-04-15 23:11:08 -0400

    functionality: display one-by-one

--- simulation_main.py
+++ simulation_main.py
@@ -1,4 +1,4 @@
-from util import stochastic_round, waterfilling
+from util import waterfilling, stochastic_round
 
 import numpy as np
 import math
@@ -11,12 +11,18 @@ def debug(s):
         print(s)
 
 class Flow:
+    """
+    Represents a flow in the network, with a unique ID, RTT, and resources used.
+    """
     def __init__(self, id, RTT, resources_used):
         self.id = id
         self.RTT = RTT
         self.resources_used = resources_used
 
 class WorldModel:
+    """"
+    Represents the world model, which maps the number of connections to the throughput.
+    """
     def __init__(self, users, resource_capacities):
         self.users = users
         self.resources = resource_capacities
@@ -28,15 +34,14 @@ class WorldModel:
                 self.incidence_matrix[i][j] = 1
 
     def tput(self, n):
-        # debug(f"n: {n}")
-        # debug(f"Weights: {weights}")
-        # debug(f"Resources: {self.resources}")
-        # debug(f"Incidence Matrix: {self.incidence_matrix}")  
         weights = [n[i]/self.users[i].RTT for i in range(len(n))]
         return waterfilling(weights, self.resources, self.incidence_matrix)
 
 
 class Optimizer:
+    """
+    FTS Optimizer Object. Controls the knob n, the number of connections for each user.
+    """
     def __init__(self, users, resources, operator_weights):
         self.users = users
         self.resources = resources
@@ -48,22 +53,24 @@ class Optimizer:
             for resource in user.resources_used:
                 self.resource_users[resource].append(i)
 
-        # # No Access
-        # - resource caps
-
     def set_decision(self, decision):
+        """"
+        Set the decision for the optimizer, the number of connections for each user.
+        """
         self.n = decision
     
     def get_decision(self):
         return self.n
 
     def update(self, new_throughput):
+        """
+        MAIN LOGIC OF OPTIMIZER. 
+        Given the new throughput, update the number of connections.
+        """
         n = self.n.copy()
 
-        print(n)
-        if(sum(n) <= 1000):
-            # Double all connections
-            print("DOUBLE")
+        # Additive Increase
+        if(sum(n) <= 200):
             return [x+2 for x in n]
 
         # Construct usage allocation for each user
@@ -80,15 +87,20 @@ class Optimizer:
 
             # Reduce the number of connections for the max user
             if (n[max_user] >= 2):
+                # Different Update Algorithms: 
                 # n[max_user] -= math.ceil(math.sqrt(n[max_user]))
                 # n[max_user] -= 3
-                n[max_user] -= math.ceil(n[max_user]*0.02)
+
+                alpha = 0.02
+                # n[max_user] -= stochastic_round(n[max_user]*alpha)
+                # n[max_user] -= math.ceil(n[max_user]*alpha)
+                n[max_user] -= alpha * n[max_user]
         return n
 
 
 def display(throughputs, goal):
     """
-    supported values are '-', '--', '-.', ':', 'None', ' ', '', 'solid', 'dashed', 'dashdot', 'dotted'
+    Displays the throughput evolution vs. goal throughput.
     """    
     # Display the throughputs as three separate lines on a curve
     plt.plot(throughputs, marker='o', linestyle='None', markersize=0.5)
@@ -96,10 +108,26 @@ def display(throughputs, goal):
     # As well as the expected throughputs
     for i in range(len(goal)):
         plt.axhline(y=goal[i], color=f'C{i}', linestyle=':', label='Goal {i}')
+        # Supported linestyles are:
+        # '-', '--', '-.', ':', 'None', ' ', '', 'solid', 'dashed', 'dashdot', 'dotted'
 
     plt.show()
 
+def display_one_by_one(throughputs, goal):
+    """
+    Displays the throughput evolution vs. goal throughput for each flow
+    in a separate plot, each of which are shown one by one.
+    """
+    for i in range(len(throughputs[0])):
+        plt.plot([throughput[i] for throughput in throughputs], marker='o', linestyle='None', markersize=0.5)
+        plt.axhline(y=goal[i], color=f'C{i}', linestyle=':', label='Goal {i}')
+        plt.show()
+
 def regret(throughputs, actual):
+    """
+    Returns the regret of the throughputs, a list of throughput vector
+    compared to the (single) expected throughput vector.
+    """
     def error(throughput, actual):
         """
         Euclidean distance between throughput vector and actual vector
@@ -109,12 +137,20 @@ def regret(throughputs, actual):
     # Sum of errors for each slice in throughputs, comparing throughputs[i] to actual
     return sum(error(throughputs[i], actual) for i in range(len(throughputs)))
 
+def avg_to_ideal(throughputs, actual):
+    # Compute average throughput:
+    avg = [sum(throughputs[i][j] for i in range(len(throughputs))) / len(throughputs) for j in range(len(throughputs[0]))]
+    return avg, regret([avg], actual)
+
 
 def simulate(WorldModel, optimizer, expected_throughputs):
+    """
+    Simulates the network, given the WorldModel and Optimizer objects.
+    """
     print("Starting Simulation")
 
     # Initialize Number of Connections
-    n = [1,1,1]
+    n = [1 for _ in WorldModel.users]
     optimizer.set_decision(n)
 
     # Run the simulation for 2000 iterations
@@ -133,9 +169,17 @@ def simulate(WorldModel, optimizer, expected_throughputs):
 
         print(f"outside n: {n}")
     
-    display(throughput_history, list(expected_throughputs.values()))
-    regret_value = regret(throughput_history, list(expected_throughputs.values()))
+    # Postprocessing
+    if isinstance(expected_throughputs, dict):
+        expected_throughputs = list(expected_throughputs.values())
+
+    display(throughput_history, expected_throughputs)
+    display_one_by_one(throughput_history, expected_throughputs)
+    regret_value = regret(throughput_history, expected_throughputs)
     print(f"regret_value: {regret_value}")
+
+    avg, avg_regret = avg_to_ideal(throughput_history, expected_throughputs)
+    print(f"avg: {avg} error: {avg_regret}")
 
 
 def main():
